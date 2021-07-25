20701edbfac407e9a2f92cf10d85dbae1c3800b5
timdh94 <timothyh94@gmail.com>
2021-07-25 09:33:14 -0400

    CRKBD:mymap -- shuffled {} [] +- = keys around

--- keyboards/crkbd/keymaps/mymap/keymap.c
+++ keyboards/crkbd/keymaps/mymap/keymap.c
@@ -49,9 +49,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                      KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_TILD,  KC_GRV,
+      KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                      XXXXXXX, KC_EQL, KC_LCBR, KC_RCBR, KC_TILD,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                      KC_PLUS, KC_MINS, KC_EQL, KC_RCBR, KC_BSLS, KC_PIPE,
+      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                      KC_PLUS, KC_MINS, KC_LBRC, KC_RBRC, KC_BSLS, KC_PIPE,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   MO(3),  KC_SPC,     KC_BSPC, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
